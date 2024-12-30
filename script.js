50d34fa9033e620bdfc8878878eb3ebef76ad552
Matteo Fusconi <matteo.fusconi@datavision.cz>
2024-12-30 12:57:25 +0100

    Cards Arrow ok

--- script.js
+++ script.js
@@ -2,13 +2,14 @@
 document.querySelectorAll('.expand-button').forEach(button => {
     button.addEventListener('click', () => {
       const cardDetails = button.parentElement.nextElementSibling;
+      const icon = button.querySelector('i');
       const isVisible = cardDetails.style.display === 'block';
   
       // Toggle visibility
       cardDetails.style.display = isVisible ? 'none' : 'block';
   
-      // Toggle button text
-      button.textContent = isVisible ? '⬇' : '⬆';
+      // Toggle button icon
+      icon.classList.toggle('fa-chevron-down', isVisible);
+      icon.classList.toggle('fa-chevron-up', !isVisible);
     });
   });
-  
